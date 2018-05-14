ffc82a6e95fdbf8e76180013fc79e993cead788a
Namila Bandara <namila007@gmail.com>
2018-05-14 22:13:33 +0530

    showing last 3 messages :laugh:

--- background/main.js
+++ background/main.js
@@ -1,14 +1,11 @@
-/*global getAccessToken*/
+/*main back to get th*/
 
 var token
-// function notifyUser(user) {
-//   browser.notifications.create({
-//     "type": "basic",
-//     "title": "Google info",
-//     "message": `Hi ${user.name}`
-//   });}
 
+const INBOX_READ =
+`https://api.stackexchange.com/2.2/me/inbox?page=1&pagesize=3&key=${KEY}&site=stackoverflow&filter=!LURAJLCc5nUZDmmWGEXuwH`
 
+//error log function
 function logError(error) {
   console.error(`Error: ${error}`);
 }
@@ -20,14 +17,46 @@ function recivemessage (message,sender,sendResponse){
     console.log(message.data)
     token = validate(message.data).catch(logError).then((val)=>{
       console.log("val "+val)
+      token = val;
       sendResponse({response: val})
       
-    })
-    
-    
+    }).then(()=>{
+      browser.notifications.create({
+        "type": "basic",
+        "title": "Token Created",
+        "message": "Token is created"
+      })
+    },logError)
     return true
-    
   }
+    if(message.command == "inbox") {
+      var url = INBOX_READ+`&access_token=${message.token}`
+      var msg = get(url, (msg1)=>{
+        console.log(msg1)
+        sendResponse({response: msg1})
+      })
+      
+      
+      return true
+     
+    }
+    
+    function get(url, callback){
+      var xmlHttp =  new XMLHttpRequest();
+      // xmlHttp.open( "GET", url, false ); // false for synchronous request
+      // xmlHttp.send( null );
+      // return xmlHttp.responseText;
+      xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+        }
+      xmlHttp.open("GET", url, true); // true for asynchronous 
+      xmlHttp.send(null);
+    }
+    
+    // return true
+    
+  
   
 }
 
--- popup/stackpopup.js
+++ popup/stackpopup.js
@@ -4,14 +4,15 @@
 const REDIRECT_URL = browser.identity.getRedirectURL();
 const CLIENT_ID = "12428";
 const KEY = "f26RUH3uoCiokrEYNeDf9Q(("
-const SCOPES = ["read_inbox", "noexpire"];
+const SCOPES = ["read_inbox"];
 const AUTH_URL =
-`https://stackoverflow.com/oauth/dialog?client_id=${CLIENT_ID}&key=${KEY}&redirect_uri=${REDIRECT_URL}`;
-//&scope=${encodeURIComponent(SCOPES.join(' '))}`;
+`https://stackoverflow.com/oauth/dialog?client_id=${CLIENT_ID}&key=${KEY}&redirect_uri=${REDIRECT_URL}&scope=${encodeURIComponent(SCOPES.join(' '))}`;
 
 
 function handleResponse(message) {
-    console.log(`background script sent a response: ${message.response}`);
+    //console.log(`background script sent a response: ${message.response}`)
+    getinbox(message.response)
+    
   }
   
   function handleError(error) {
@@ -32,11 +33,32 @@ document.getElementById('tokenbtn').addEventListener("click", async function get
 
 })
 
-function tokenSet (req) {
-    document.getElementById('token').innerHTML = req
+
+function handleinbox(message) {
+    console.log("Inbox")
     
+    let msg = JSON.parse(message.response)
+ 
+   
+    var inbox = document.getElementById("inbox")
+
+    for (var i=0; i<msg.items.length; i++){
+       var item = document.createElement('li')
+        item.innerHTML =
+        '<a href="'+ msg.items[i].link +'">' + msg.items[i].title + '</a>'
+        inbox.appendChild(item)
+    }
+    
+   
 }
 
+function getinbox(token) {
+    var inbox = browser.runtime.sendMessage({
+        command: "inbox",
+        token: token
+    })
+    inbox.then(handleinbox,handleError)
+}
 
 
 
